04208b5494dd836fb87d940d092daf08419336ae
Borrax <borrax@protonmail.com>
2023-12-30 23:43:38 +0200

    Removing the GUI for now and initializing VAD from main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
+import pyaudio
+from vad import Vad
+
+RECORD_TIME = 0.06
+FRAME_RATE = 16000
+BYTES = pyaudio.paInt16
+CHUNK = int(FRAME_RATE * RECORD_TIME)
+
+audio = pyaudio.PyAudio()
+input_stream = audio.open(
+    format=BYTES,
+    channels=1,
+    rate=FRAME_RATE,
+    frames_per_buffer=CHUNK,
+    input=True
+)
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle('Ryber')
-
-
-app = QApplication(sys.argv)
-window = MainWindow()
-window.show()
-
-app.exec()
+vad = Vad(RECORD_TIME, FRAME_RATE, CHUNK)
+while True:
+    vad.has_voice(input_stream)
--- vad.py
+++ vad.py
@@ -59,23 +59,3 @@ class Vad:
             return True
 
         return False
-
-
-RECORD_TIME = 0.06
-FRAME_RATE = 16000
-BYTES = pyaudio.paInt16
-CHUNK = int(FRAME_RATE * RECORD_TIME)
-
-audio = pyaudio.PyAudio()
-input_stream = audio.open(
-    format=BYTES,
-    channels=1,
-    rate=FRAME_RATE,
-    frames_per_buffer=CHUNK,
-    input=True
-)
-
-
-vad = Vad(RECORD_TIME, FRAME_RATE, CHUNK)
-while True:
-    vad.has_voice(input_stream)
