859e1b60a05030a89036f1a9fa040f51d4d7577a
Borrax <borrax@protonmail.com>
2024-01-11 22:09:03 +0200

    Reconfiguring the scraper and finding better the info in google

--- modules/controllers/get_info_handler.py
+++ modules/controllers/get_info_handler.py
@@ -6,7 +6,7 @@ from config import ROOT_DIR
 # the maximum time a locator can wait before timinout
 TIMEOUT_TIME = 5    # in ms
 SCREENSHOT_PATH = os.path.join(
-    ROOT_DIR, 'controllers/temp/search_screenshot.png')
+    ROOT_DIR, 'modules/controllers/temp/search_screenshot.png')
 
 
 def get_info_handler(raw_query):
@@ -17,8 +17,9 @@ def get_info_handler(raw_query):
         browser = p.chromium.launch()
         device = p.devices['Desktop Edge']
         context = browser.new_context(
-            locale='de-DE',
+            locale='bg-BG',
             color_scheme='dark',
+            timezone_id='Europe/Sofia',
             forced_colors='active',
             **device
         )
@@ -43,17 +44,10 @@ def get_info_handler(raw_query):
 
 
 def get_heading_text(page):
-    selector = '#rso > div:nth-child(2) > div > block-component > div > div.dG2XIf.XzTjhb > div > div > div > div > div.ifM9O > div > div > div > div > div.wDYxhc > div > span > span'
-    info_locator = page.locator(selector)
+    selector = 'div.wDYxhc > div > span'
+    info_locator = page.locator(selector).all()
     try:
-        return info_locator.inner_text(timeout=TIMEOUT_TIME)
-    except Exception:
-        pass
-
-    selector = 'div.wDYxhc > div > span > span'
-    info_locators = page.locator(selector).nth(0)
-    try:
-        return info_locators.inner_text(timeout=TIMEOUT_TIME)
+        return info_locator[0].inner_text(timeout=TIMEOUT_TIME)
     except Exception:
         pass
 
@@ -61,16 +55,11 @@ def get_heading_text(page):
 
 
 def get_side_info(page):
-    selector = '.xGj8Mb > .wDYxhc .kno-rdesc span'
+    selector = 'div.wDYxhc span:nth-child(1)'
 
-    info_locators = page.locator(selector).nth(0)
+    info_locators = page.locator(selector).all()
+    print(len(info_locators))
     try:
-        return info_locators.inner_text(timeout=TIMEOUT_TIME)
+        return info_locators[4].inner_text(timeout=TIMEOUT_TIME)
     except Exception:
         return None
-
-
-# print(get_info_handler('Tell me about quantum physics.'))
-# print(get_info_handler('who is bill gates?'))
-# print(get_info_handler('when is bill gates born'))
-# print(get_info_handler('Can you tell me how stars are born?'))
