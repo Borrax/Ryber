0cc723f44a6bc22a4e12a43d0d03b202c17f216d
Borrax <borrax@protonmail.com>
2024-01-18 23:37:56 +0200

    Moving the AI-UI signaller to its own file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from modules.view.view import GUI, AIViewSignaller
+from modules.view.view import GUI
+from modules.view.ai_ui_signaller import AIViewSignaller
 
 if __name__ == '__main__':
     ui_ai_signaller = AIViewSignaller()
--- modules/view/view.py
+++ modules/view/view.py
@@ -1,21 +1,7 @@
 import sys
 from PySide6 import QtCore, QtWidgets
-from PySide6.QtCore import Signal, Slot, QObject
+from PySide6.QtCore import Slot
 from modules.controllers.assistant_controller import AssistantController
-
-
-class AIViewSignaller(QObject):
-    response_signal = Signal(str)
-    loading_app_signal = Signal(bool)
-
-    def __init__(self):
-        super().__init__()
-
-    def update_response_text(self, new_text):
-        self.response_signal[str].emit(new_text)
-
-    def update_app_loading(self, is_loading):
-        self.loading_app_signal[bool].emit(is_loading)
 
 
 class AssistantThread(QtCore.QThread):
